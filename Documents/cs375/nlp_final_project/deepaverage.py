e94b2dc339b7d61cd0a6b2d13679fc9c23e107e9
alessa somer <aas10@williams.edu>
2023-05-08 13:15:33 -0400

    move content to deepaverage.py

--- Documents/cs375/nlp_final_project/deepaverage.py
+++ Documents/cs375/nlp_final_project/deepaverage.py
@@ -1,7 +1,10 @@
+from gensim.models.keyedvectors import KeyedVectors
+import json
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
-from gensim.models.keyedvectors import KeyedVectors
+import pandas as pd
+from nltk.tokenize import TweetTokenizer
 
 class DeepAveragingNetwork(nn.Module):
     """
@@ -81,6 +84,7 @@ class DeepAveragingNetwork(nn.Module):
         log_probs = self.log_softmax(out)
         print(log_probs.shape)
         return log_probs
+    
     def train_model(self, X_train, Y_train, X_dev, Y_dev, loss_fn, optimizer, num_iterations, batch_size = 500, check_every=10, verbose=False): 
         """
         Method to train the model. 
@@ -163,3 +167,37 @@ class DeepAveragingNetwork(nn.Module):
         Calculates accuracy. No need to modify this method. 
         """
         return np.mean(y_pred == y_true)
+    
+def main():
+    LEARNING_RATE = 1e-1
+    HIDDEN_DIM1 = 200 
+    HIDDEN_DIM2 = 100
+    LEAKY_RELU_NEG_SLOPE = 0.01
+    DROPOUT_PROB = 0.4 
+    from preprocess import Twitter
+    twitter = Twitter()
+    embed_array = twitter.create_embeddings()
+    #print(tweet_examples_test[0][0:5])
+
+    from deepaverage import DeepAveragingNetwork
+    model = DeepAveragingNetwork(2,embed_array,50, 20, 20, 0.01,0.1)
+    loss_fn= nn.NLLLoss() #For binary logistic regression 
+    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+    #Random inputs to check syntax
+    #X_batch = torch.randint(low=0, high=len(vocab2indx), size=(100, 10))
+    #print(X_batch.shape)
+    #log_probs_out = model.forward(X_batch)
+    NUMBER_ITERATIONS = 300
+    X_train = twitter.create_Xtrain_tensor()
+    print("x train:")
+    X_dev = twitter.create_Xdev_tensor()
+    loss_history, train_accuracy, dev_accuracy = model.train_model(X_train, twitter.get_Y_train(), X_dev, twitter.get_Y_dev(), 
+                                                             loss_fn, optimizer, NUMBER_ITERATIONS, 
+                                                             batch_size = 200,
+                                                             check_every=50, verbose=False)
+    print(loss_history)
+    print(train_accuracy)
+    print(dev_accuracy)
+
+if __name__ == '__main__':
+    main()
